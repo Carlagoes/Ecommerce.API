a35445e4fad592008bfbffa89e690c8e54bcd56a
Carlagoes <carllagoess@gmail.com>
2023-08-03 19:22:30 -0300

    Add project files.

---
+++ Ecommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,80 @@
+﻿using Ecommerce.API.Models;
+using Ecommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        //bc
+        private IUsuarioRepository _repository;
+
+        public UsuariosController()
+        {
+            _repository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+            if(usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody]Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
+
+//CRUD
+// - GET: obter a lista de usuários
+// - GET: obter o usuário passando o ID
+// - POST: Cadastrar um usuário 
+// - PUT: Atualizar um usuário 
+// - DELETE: Remover um usuário 
+
+//Metodo HTTP
+//Ex Get(): www.minhaApi.com.br/api/Usuario
+//Ex Get(Usuario): www.minhaApi.com.br/api/Usuario/2
+
+//Retornos
+//200 ok
+//300 redirecionamento para outro endereço
+//400 erro dados por parte do usuario
+//404 not found 
+//500 servidor, exception
---
+++ Ecommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,97 @@
+﻿using Ecommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private IDbConnection _connection;
+
+        public UsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EcommerceAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public List<Usuario> Get()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM Usuario";
+                command.Connection = (SqlConnection)_connection;
+                
+                _connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.RG = dataReader.GetString("RG");
+                    usuario.CPF = dataReader.GetString("CPF");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+
+                }
+            }
+            finally 
+            {
+                _connection.Close();
+            }
+
+            return usuarios;
+        }
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(u => u.Id == id);
+        }
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+            if(ultimoUsuario == null)
+            {
+                usuario.Id = 1;
+            }
+            else
+            {
+                usuario.Id = ultimoUsuario.Id;
+                usuario.Id++;
+            }
+            _db.Add(usuario);   
+        }
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(u => u.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(u => u.Id == id));
+        }
+
+        //bc
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1, Nome="Filipe", Email="filipe@gmail.com"},
+            new Usuario(){ Id=2, Nome="Marcelo", Email="marcelo@gmail.com"},
+            new Usuario(){ Id=3, Nome="Jessica", Email="jessica@gmail.com"},
+        
+        };
+    }
+}
+
