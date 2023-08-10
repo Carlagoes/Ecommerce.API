f439ac90c34ce58d5a465dc69d67aade4073ba87
Carlagoes <carllagoess@gmail.com>
2023-08-09 21:22:56 -0300

    ajuste usuarioRepository

--- Ecommerce.API/Repositories/UsuarioRepository.cs
+++ Ecommerce.API/Repositories/UsuarioRepository.cs
@@ -1,10 +1,7 @@
 ﻿using Ecommerce.API.Models;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ecommerce.API.Repositories
 {
